0db717932569da6896b235d8d705e75d15db9f56
Gusnand <gusnanda1170@gmail.com>
2025-03-08 12:16:26 +0800

    durasi satu jam tujuh belas menit

--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -1,24 +1,20 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import React from "react";
 
-export default function Index({ projects }) {
+export default function Index() {
   return (
     <AuthenticatedLayout
       header={
         <h2 className="text-xl font-semibold leading-tight text-gray-800">
-          Projects
+          Your Tasks Here
         </h2>
       }
     >
-      <Head title="Projects" />
-
+      <Head title="Your Tasks" />
       <div className="py-12">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900">
-              <pre>{JSON.stringify(projects, undefined, 2)}</pre>
-            </div>
+            <div className="p-6 text-gray-900">Your Tasks Here</div>
           </div>
         </div>
       </div>
